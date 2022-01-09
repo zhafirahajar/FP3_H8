0dd0ddb0e093563fcddd1974371096d0bac17c0e
Zhafirahajar <zfrhjr@gmail.com>
2022-01-10 00:49:28 +0700

    fix: register error message

--- libs/resLibs.js
+++ libs/resLibs.js
@@ -6,7 +6,22 @@ class resLibs {
 	}
 
 	static resError(res, code, err) {
-		return res.status(code).json(err);
+		let errCode = 500,
+			errMessages = [];
+
+		if (err.name.includes("Sequelize")) {
+			errCode = 400;
+		}
+
+		for (let index in err.errors) {
+			let msg = err.errors[index].message;
+			errMessages.push(msg);
+		}
+
+		return res.status(errCode).json({
+			error: err.name,
+			message: errMessages,
+		});
 	}
 }
 
