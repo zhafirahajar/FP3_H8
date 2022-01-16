6325d6310cc41f19de8e0e90c1e55b5f8ed6cbdb
Zhafirahajar <zfrhjr@gmail.com>
2022-01-16 09:12:43 +0700

    feat: add validation on product model ;; fix: login user

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,23 @@
 const route = require("express").Router();
 const userControllers = require("../controllers/userControllers");
-const categoryControllers = require("../controllers/categoryControllers")
-
-
+const categoryControllers = require("../controllers/categoryControllers");
+const loginMiddleware = require("../libs/authenticationLibs");
 
 // USER REGIS - LOGIN ROUTE
-//route.post("/users/register", userControllers.register);
-//route.post("/users/login", userControllers.login);
+route.post("/users/register", userControllers.register);
+route.post("/users/login", userControllers.login);
 
 // MIDDLEWARE CHECK USER
-// route.use(userControllers.loginMiddleware);
+route.use(loginMiddleware);
 
 // USER MANAGEMENT ROUTE
 // route.put("/users/:userId", userControllers.edit);
 // route.delete("/users/:userId", userControllers.delete);
 
-
 //MIDDLEWARE CHECK CATEGORY
-route.post("/categories", categoryControllers.create)
-route.get("/categories", categoryControllers.index)
-route.patch("/categories/:categoryId", categoryControllers.update)
-route.delete("/categories/:categoryId", categoryControllers.delete)
-
-
+route.post("/categories", categoryControllers.create);
+route.get("/categories", categoryControllers.index);
+route.patch("/categories/:categoryId", categoryControllers.update);
+route.delete("/categories/:categoryId", categoryControllers.delete);
 
 module.exports = route;
