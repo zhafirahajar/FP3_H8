d12a0366c7001e7ee383eec2adb0fdae3461c423
Zhafirahajar <zfrhjr@gmail.com>
2022-01-20 20:37:00 +0700

    fix: create transaction

--- controllers/transactionControllers.js
+++ controllers/transactionControllers.js
@@ -8,126 +8,117 @@ const productLibs = require("../libs/productLibs");
 const balanceLibs = require("../libs/balanceLibs");
 const RPGen = require("../libs/balanceLibs");
 
-class transactionControllers{
-    static async create(req, res){
-        let user_login = jwt.verify(req.headers.token, SECRET_KEY)
-        let user_instance = await userLibs.getById(user_login.id);
-        let { produkId, quantity} = req.body;
-        let produk_instance = await productLibs.getById(produkId)
-        let checkStock = await productLibs.checkStock(res, quantity, produkId)
-        let hargaBeli = parseInt(quantity) * parseInt(produk_instance.price)
-        if(checkStock === true){
-            let reduceBalance = balanceLibs.reduceBalance(res, hargaBeli, user_instance)
-            let updateStock = productLibs.updateStock(res, parseInt(quantity), produk_instance.id)
-        }
-        
-       
-            /*
+class transactionControllers {
+	static async create(req, res) {
+		let user_login = jwt.verify(req.headers.token, SECRET_KEY);
+		let user_instance = await userLibs.getById(user_login.id);
+		let { productId, quantity } = req.body;
+		let produk_instance = await productLibs.getById(productId);
+		let checkStock = await productLibs.checkStock(res, quantity, productId);
+		let hargaBeli = parseInt(quantity) * parseInt(produk_instance.price);
+		if (checkStock.isStocked == true) {
+			let reduceBalance = await balanceLibs.reduceBalance(res, hargaBeli, user_instance);
+			let updateStock = await productLibs.updateStock(res, parseInt(quantity), produk_instance.id);
+		}
+
+		/*
                 - kurangi balance
                 - tambah product terjual di kategory
                 - kurangi stok barang
             */
-            
 
-            
-            let input = {
-                ProductId : parseInt(produkId),
-                UserId : user_instance.id,
-                quantity : parseInt(quantity),
-                total_price : hargaBeli
-            }
+		let input = {
+			ProductId: parseInt(productId),
+			UserId: user_instance.id,
+			quantity: parseInt(quantity),
+			total_price: hargaBeli,
+		};
 
-            TransactionHistory.create(input)
-            .then((data) => {
-                res.status(201).json({
-                    message : "you have succesfully purchase the product",
-                    TransactionBill : {
-                        total_price : data.total_price,
-                        quantity : data.quantity,
-                        product_name : produk_instance.title
-                    }
-                })
-            })
-            .catch((err) => {
-                let errCode = 500;
-                let errMessages = [];
+		TransactionHistory.create(input)
+			.then((data) => {
+				res.status(201).json({
+					message: "you have succesfully purchase the product",
+					TransactionBill: {
+						total_price: data.total_price,
+						quantity: data.quantity,
+						product_name: produk_instance.title,
+					},
+				});
+			})
+			.catch((err) => {
+				let errCode = 500;
+				let errMessages = [];
 
-                for (let index in err.errors) {
-                        let errMsg = err.errors[index].message;
-                        errMessages.push(errMsg);
-                }
+				for (let index in err.errors) {
+					let errMsg = err.errors[index].message;
+					errMessages.push(errMsg);
+				}
 
-                if (err.name.includes("Sequelize")) {
-                        errCode = 400;
-                }
-            })
-        
-    }
+				if (err.name.includes("Sequelize")) {
+					errCode = 400;
+				}
+			});
+	}
 
-    static async index(req, res){
-        let user_login = jwt.verify(req.headers.token, SECRET_KEY)
-        let user_instance = await userLibs.getById(user_login.id);
-     
-        TransactionHistory.findAll({
-            where : {UserId : user_instance.id},
-            attributes : {exclude : ["id"]},
-            include : {model: Product, attributes: ["id", "title", "price", "stock", "CategoryId"]}
-        })
-        .then((data)=>{
-            res.status(200).json({"transcationHistories" : data})
-        })
-        .catch((err)=>{
-            res.status(500).json(err)
-        })
-        
-    }
+	static async index(req, res) {
+		let user_login = jwt.verify(req.headers.token, SECRET_KEY);
+		let user_instance = await userLibs.getById(user_login.id);
 
-    static async admin(req, res){
-        let user_login = jwt.verify(req.headers.token, SECRET_KEY)
-        let user = await userLibs.getById(user_login.id)
-        let user_auth = await authLibs.checkUserAuth(req, res, user);
-        let isAuthenticated = user_auth.value;
-        let isAdmin = await authLibs.checkAdmin(res, user)
-    
-        TransactionHistory.findAll({
-            attributes : {exclude : ["id"]},
-            include : [
-                {model: Product, attributes: ["id", "title", "price", "stock", "CategoryId"]},
-                {model: User, attributes: ["id", "email", "balance", "gender", "role"]}
-            ]
-        })
-        .then((data)=>{
-            res.status(200).json({"transcationHistories" : data})
-        })
-        .catch((err)=>{
-            res.status(500).json(err)
-        })
-        
-    }
+		TransactionHistory.findAll({
+			where: { UserId: user_instance.id },
+			attributes: { exclude: ["id"] },
+			include: { model: Product, attributes: ["id", "title", "price", "stock", "CategoryId"] },
+		})
+			.then((data) => {
+				res.status(200).json({ transcationHistories: data });
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
 
-    static async getOne(req, res){
-        let user_login = jwt.verify(req.headers.token, SECRET_KEY)
-        let user = await userLibs.getById(user_login.id)
-        let user_auth = await authLibs.checkUserAuth(req, res, user);
-        let isAuthenticated = user_auth.value;
-        let isAdmin = await authLibs.checkAdmin(res, user)
-     
-        TransactionHistory.findOne({
-            where : {id : req.params.transactionsId},
-            attributes : {exclude : ["id"]},
-            include : {model: Product, attributes: ["id", "title", "price", "stock", "CategoryId"]},
-            plain : true
-        })
-        .then((data)=>{
-            res.status(200).json({data})
-        })
-        .catch((err)=>{
-            res.status(500).json(err)
-        })
-        
-    }
+	static async admin(req, res) {
+		let user_login = jwt.verify(req.headers.token, SECRET_KEY);
+		let user = await userLibs.getById(user_login.id);
+		let user_auth = await authLibs.checkUserAuth(req, res, user);
+		let isAuthenticated = user_auth.value;
+		let isAdmin = await authLibs.checkAdmin(res, user);
 
+		TransactionHistory.findAll({
+			attributes: { exclude: ["id"] },
+			include: [
+				{ model: Product, attributes: ["id", "title", "price", "stock", "CategoryId"] },
+				{ model: User, attributes: ["id", "email", "balance", "gender", "role"] },
+			],
+		})
+			.then((data) => {
+				res.status(200).json({ transcationHistories: data });
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
+
+	static async getOne(req, res) {
+		let user_login = jwt.verify(req.headers.token, SECRET_KEY);
+		let user = await userLibs.getById(user_login.id);
+		let user_auth = await authLibs.checkUserAuth(req, res, user);
+		let isAuthenticated = user_auth.value;
+		let isAdmin = await authLibs.checkAdmin(res, user);
+
+		TransactionHistory.findOne({
+			where: { id: req.params.transactionsId },
+			attributes: { exclude: ["id"] },
+			include: { model: Product, attributes: ["id", "title", "price", "stock", "CategoryId"] },
+			plain: true,
+		})
+			.then((data) => {
+				res.status(200).json({ data });
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
 }
 
-
-module.exports = transactionControllers
+module.exports = transactionControllers;
