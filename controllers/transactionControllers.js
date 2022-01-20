34c10766f810854f7ffefce6e6a2e8d4ff186c3e
Zhafirahajar <zfrhjr@gmail.com>
2022-01-20 21:35:41 +0700

    fix: create transaction & product's stock limit on creation

--- controllers/transactionControllers.js
+++ controllers/transactionControllers.js
@@ -7,57 +7,64 @@ const resLibs = require("../libs/resLibs");
 const productLibs = require("../libs/productLibs");
 const balanceLibs = require("../libs/balanceLibs");
 const RPGen = require("../libs/balanceLibs");
+const { ReadyForQueryMessage } = require("pg-protocol/dist/messages");
 
 class transactionControllers {
 	static async create(req, res) {
+		// authentication
 		let user_login = jwt.verify(req.headers.token, SECRET_KEY);
 		let user_instance = await userLibs.getById(user_login.id);
+
+		//get product and price
 		let { productId, quantity } = req.body;
 		let produk_instance = await productLibs.getById(productId);
-		let checkStock = await productLibs.checkStock(res, quantity, productId);
 		let hargaBeli = parseInt(quantity) * parseInt(produk_instance.price);
-		if (checkStock.isStocked == true) {
+
+		// check product's stock
+		let checkStock = await productLibs.checkStock(res, quantity, productId);
+
+		if (checkStock.isStocked) {
 			let reduceBalance = await balanceLibs.reduceBalance(res, hargaBeli, user_instance);
-			let updateStock = await productLibs.updateStock(res, parseInt(quantity), produk_instance.id);
+
+			// check user's balance
+			if (reduceBalance.status == false) {
+			} else {
+				await productLibs.updateStock(res, parseInt(quantity), produk_instance.id);
+
+				let input = {
+					ProductId: parseInt(productId),
+					UserId: user_instance.id,
+					quantity: parseInt(quantity),
+					total_price: hargaBeli,
+				};
+
+				TransactionHistory.create(input)
+					.then((data) => {
+						let totalRP = RPGen.rupiahGenerator(data.total_price);
+						res.status(201).json({
+							message: "you have succesfully purchase the product",
+							TransactionBill: {
+								total_price: totalRP,
+								quantity: data.quantity,
+								product_name: produk_instance.title,
+							},
+						});
+					})
+					.catch((err) => {
+						let errCode = 500;
+						let errMessages = [];
+
+						for (let index in err.errors) {
+							let errMsg = err.errors[index].message;
+							errMessages.push(errMsg);
+						}
+
+						if (err.name.includes("Sequelize")) {
+							errCode = 400;
+						}
+					});
+			}
 		}
-
-		/*
-                - kurangi balance
-                - tambah product terjual di kategory
-                - kurangi stok barang
-            */
-
-		let input = {
-			ProductId: parseInt(productId),
-			UserId: user_instance.id,
-			quantity: parseInt(quantity),
-			total_price: hargaBeli,
-		};
-
-		TransactionHistory.create(input)
-			.then((data) => {
-				res.status(201).json({
-					message: "you have succesfully purchase the product",
-					TransactionBill: {
-						total_price: data.total_price,
-						quantity: data.quantity,
-						product_name: produk_instance.title,
-					},
-				});
-			})
-			.catch((err) => {
-				let errCode = 500;
-				let errMessages = [];
-
-				for (let index in err.errors) {
-					let errMsg = err.errors[index].message;
-					errMessages.push(errMsg);
-				}
-
-				if (err.name.includes("Sequelize")) {
-					errCode = 400;
-				}
-			});
 	}
 
 	static async index(req, res) {
